7225dfc4a65916888c5e40b6e1658fcbf25a5f97
Erik Zapata <zapatabrandon@gmai.com>
2022-11-22 21:41:17 -0500

    Update bot to use objects and events from discordjs v14

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const { prefix, token } = require('./config.json');
+const { Client, GatewayIntentBits, Events } = require('discord.js');
+const client = new Client({ intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessages
+]});
+const { prefix, token, CLIENT_ID } = require('./config.json');
 
-client.on('message', message => {
+client.on(Events.MessageCreate, (message) => {
     if (!message.content.startsWith(prefix) || message.author.bot ) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
     if (command === "shuffle") {
-        const userMentions = message.mentions.users.array();
+        const userMentions = message.mentions.members;
         const users = [...new Set(userMentions)];
 
         if (users.length <= 1) {
             return message.channel.send("You must have at least 2 mentions to start the shuffle.");
         }
+
         else {
             const pairs = getPairs(users);
-            pairs.forEach(function(value, key) {
-                key.send("Hey you dingus, you're the secret santa for " + value.username);
+            pairs.forEach(function(key, value) {
+                // user member objects are made up of `[ID, GUILD_MEMBER]`
+                client.users.send(key[0], "I'm so sorry. It's been a long day and discord changed a ton of their API recently. Get a gift for: " + value[1].user.username);
             });
 
             return message.channel.send("The santas have been sent. Enjoy!");
